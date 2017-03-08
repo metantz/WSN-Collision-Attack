54ddbb79e3605844ecbce6d896f708ae94f0c564
Jacopo Cavallo <jacopo.cavallo@protonmail.com>
2017-03-08 12:29:16 +0000

    Create Attacker.h

---
+++ AttackerCC2420/Attacker.h
@@ -0,0 +1,12 @@
+#ifndef ATTACKER_H
+#define ATTACKER_H
+
+// packet's payload structure
+typedef nx_struct AttackerMsg		
+{
+	nx_int16_t someStuff1;
+	nx_int16_t someStuff2;
+
+}AttackerMsg;
+
+#endif
